704c511e96de3a425baac554484c3cbfbb757977
stubbornnessness <1219721814yxj>
2016-10-10 22:32:12 +0800

    3.2版本开始支持carthage

---
+++ Carthage/Build/iOS/MJRefresh_iOS.framework/Headers/MJRefresh_iOS.h
@@ -0,0 +1,19 @@
+//
+//  MJRefresh_iOS.h
+//  MJRefresh_iOS
+//
+//  Created by yuanxiaojun on 2016/10/10.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJRefresh_iOS.
+FOUNDATION_EXPORT double MJRefresh_iOSVersionNumber;
+
+//! Project version string for MJRefresh_iOS.
+FOUNDATION_EXPORT const unsigned char MJRefresh_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJRefresh_iOS/PublicHeader.h>
+
+
